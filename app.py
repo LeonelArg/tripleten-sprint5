29e221189aee47250c3efc0128f6f829169ecf2c
LeonelArg <leonel-k@live.com>
2024-07-31 20:13:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -26,3 +26,18 @@ if scatter_button: # al hacer clic en el botón
 
     # Mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
+
+show_histogram = st.checkbox('Mostrar histograma')
+show_scatter = st.checkbox('Mostrar gráfico de dispersión')
+
+# Mostrar el histograma si se selecciona la casilla de verificación correspondiente
+if show_histogram:
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+    hist_fig = px.histogram(car_data, x="odometer", title="Histograma del odómetro")
+    st.plotly_chart(hist_fig, use_container_width=True)
+
+# Mostrar el gráfico de dispersión si se selecciona la casilla de verificación correspondiente
+if show_scatter:
+    st.write('Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
+    scatter_fig = px.scatter(car_data, x="odometer", y="price", color="year", title="Odometer vs Price by Year")
+    st.plotly_chart(scatter_fig, use_container_width=True)
